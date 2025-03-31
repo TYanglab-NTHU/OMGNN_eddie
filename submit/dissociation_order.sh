a77de7ff0dedbdb5c58bfa75a70913cfea994bf9
qteddie <40942101S@ntnu.edu.tw>
2025-03-31 15:30:51 +0800

    update submit folder

--- submit/dissociation_order.sh
+++ submit/dissociation_order.sh
@@ -10,4 +10,4 @@
 #SBATCH --output=../logs/job_output_%j.txt
 #SBATCH --error=../logs/job_error_%j.txt   
 
-python integrated_nist_converter.py --input ../data/NIST_database_onlyH_6TypeEq_pos_match_max_fg_other.csv --output ../data/nist_pka_data.csv
+python -u ../src/dissociation_order.py
